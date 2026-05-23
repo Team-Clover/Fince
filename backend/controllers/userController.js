dd2503ad19ecb47ff08e2bfa70261cc1740f9218
Tanmoy05 <tanmoymukherjee746@gmail.com>
2026-05-23 12:45:50 +0530

    User Model Add

---
+++ backend/controllers/userController.js
@@ -0,0 +1,21 @@
+
+
+const loginController = (req, res) => {
+    const { email, password } = req.body;
+
+    try{
+        if
+
+    }
+    catch(error){
+        res.status(400).json({ message: error.message });
+    }
+};
+
+
+const registerController = (req, res) => {};
+
+
+const logoutController = (req, res) => {};
+
+const profileController = (req, res) => {};

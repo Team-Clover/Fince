6d97df2e1f3c16b6bd742d9cb650996eb8beaa60
Tanmoy05 <tanmoymukherjee746@gmail.com>
2026-05-23 11:09:47 +0530

    mongouri added

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,10 @@ dotenv.config();
 
 const app = express();
 
+//mongodb connection
+import connectDB from './config/db.js';
+connectDB();
+
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
